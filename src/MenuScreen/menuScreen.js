6931e1ad8766d6234257b66bcb12c8e876dc1d47
ant-st <antsarnow@gmail.com>
2023-04-07 21:31:17 +0200

    menus state managment, adding new items to menus

--- src/MenuScreen/menuScreen.js
+++ src/MenuScreen/menuScreen.js
@@ -1,12 +1,74 @@
 import {useParams} from "react-router";
+import {useDispatch, useSelector} from "react-redux";
+import {addPositionToMenu, selectMenus} from "../Menus/menusSlice";
+import {useEffect, useState} from "react";
 
 export const MenuScreen = () => {
+    const dispatch = useDispatch();
+
     let { id } = useParams();
+    let menus = useSelector(selectMenus);
+
+    const [menuItems, setMenuItems] = useState([]);
+    const [newName, setNewName] = useState('');
+    const [newDesc, setNewDesc] = useState('');
+    const [newPrice, setNewPrice] = useState(0);
+
+    useEffect(() => {
+            for (let i=0;i<menus.length;i++) {
+                if (
+                    menus[i].id === Number(id)) {
+                    setMenuItems(menus[i].positions);
+                }
+            }
+        }, [menus, id]);
+
+    const renderMenuItems = (item) => {
+        return (
+            <div>
+                <h3>{item.name}</h3>
+                <p>{item.desc}</p>
+                <h4>{item.price}</h4>
+            </div>
+        )
+    }
+
+    const handleNameChange = ({target}) => {
+        setNewName(target.value);
+    }
+
+    const handleDescChange = ({target}) => {
+        setNewDesc(target.value);
+    }
+
+    const handlePriceChange = ({target}) => {
+        setNewPrice(target.value);
+    }
+
+    const handleSubmit = () => {
+        let newPosition = {
+            menuId: id,
+            name: newName,
+            desc: newDesc,
+            price: newPrice
+        }
+        dispatch(addPositionToMenu(newPosition));
+        console.log('Dispatched!');
+        console.log(menus);
+    }
+
     return (
         <div>
             I am a menu {id}!
-
-
+            <div id="menuPosCreator">
+                <input defaultValue="Nazwa" onChange={handleNameChange}/>
+                <input defaultValue="Opis" onChange={handleDescChange}/>
+                <input defaultValue="Cena" onChange={handlePriceChange}/>
+                <button onClick={handleSubmit}>GO!</button>
+            </div>
+            <div id="menuList">
+                {menuItems.map(renderMenuItems)}
+            </div>
         </div>
     )
 }
--- src/Menus/menusSlice.js
+++ src/Menus/menusSlice.js
@@ -2,11 +2,27 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const menusSlice = createSlice({
     name: 'menus',
-    initialState: [{id:1, name: 'Klasyczne', positions: [{name: 'Ziemniaki', desc: 'Ziemniaki z pyrami', price: 12}]}],
+    initialState: [{id:1, name: 'Klasyczne', positions: [{name: 'Ziemniaki', desc: 'Ziemniaki z pyrami', price: 12}, {name: 'Kamienie', desc: 'Kamienie z pyrami', price: 13}]}, {id:2, name: 'Sezonowe', positions: [{name: 'Ziemniaki', desc: 'Ziemniaki z pyrami', price: 12}]}],
     reducers: {
         addMenu: (state, action) => {
             state.push(action.payload);
         },
+        addPositionToMenu: (state, action) => {
+            let newPosition = {
+                name: action.payload.name,
+                desc: action.payload.desc,
+                price: action.payload.price,
+            }
+            for (let i=0;i<state.length;i++) {
+                if (state[i].id === Number(action.payload.menuId)) {
+                    console.log('inside');
+                    state[i].positions.push(newPosition);
+                    return;
+                }
+
+            }
+        },
+
 
     }
 });
@@ -15,8 +31,11 @@ export const selectMenus = (state) => {
     return state.menus;
 };
 
+
+
 export const {
-    addTable
+    addTable,
+    addPositionToMenu
 } = menusSlice.actions;
 
 export default menusSlice.reducer;
