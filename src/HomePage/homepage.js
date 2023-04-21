ac974b1f8e308e6e07758ef5372fe6c712789567
ant-st <antsarnow@gmail.com>
2023-04-21 12:23:40 +0200

    Added: access control in main menu

--- src/HomePage/homepage.js
+++ src/HomePage/homepage.js
@@ -1,30 +1,34 @@
 import './homepage.css'
 import {NavLink} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectLoggedUser} from "../users/usersSlice";
 
 export const Homepage = () => {
+        const {admin, manager, active} = useSelector(selectLoggedUser);
+        console.log(admin, manager, active);
     return (
         <div id="homepage">
-            <NavLink to={'tables'} className="menuContainer">
+            <NavLink to={active ? 'tables' : '/'} className={`menuContainer ` + (active ? '' : 'disabled')}>
                     <img src={require('./menuImg.png')} alt="menu icon"/>
                     <p>Obsługa kelnerska</p>
             </NavLink>
-            <NavLink to={'menus'} className="menuContainer">
-                    <img src={require('./menuImg.png')} alt="menu icon"/>
-                    <p>Edycja Menu</p>
-            </NavLink>
-            <NavLink to={'delivery'} className="menuContainer">
+            <NavLink to={active ? 'delivery' : '/'} className={`menuContainer ` + (active ? '' : 'disabled')}>
                     <img src={require('./menuImg.png')} alt="menu icon"/>
                     <p>Dostawy</p>
             </NavLink>
-            <NavLink to={'kitchen'} className="menuContainer">
+            <NavLink to={active ? 'kitchen' : '/'} className={`menuContainer ` + (active ? '' : 'disabled')}>
                     <img src={require('./menuImg.png')} alt="menu icon"/>
                     <p>Kuchnia</p>
             </NavLink>
-            <NavLink to={'sales'} className="menuContainer">
+                <NavLink to={admin ? 'menus' : '/'} className={`menuContainer ` + (admin ? '' : 'disabled')}>
+                        <img src={require('./menuImg.png')} alt="menu icon"/>
+                        <p>Edycja Menu</p>
+                </NavLink>
+            <NavLink to={manager ? 'sales' : '/'} className={`menuContainer ` + (manager ? '' : 'disabled')}>
                     <img src={require('./menuImg.png')} alt="menu icon"/>
                     <p>Wyniki sprzedaży</p>
             </NavLink>
-            <NavLink to={'settings'} className="menuContainer">
+            <NavLink to={admin ? 'settings' : '/'} className={`menuContainer ` + (admin ? '' : 'disabled')}>
                     <img src={require('./menuImg.png')} alt="menu icon"/>
                     <p>Ustawienia</p>
             </NavLink>
